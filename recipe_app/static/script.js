dd0220677a1926ee00c730fff39b9ac53a52d3fe
= <awadsharif9@gmail.com>
2020-08-03 00:02:07 -0700

    added ajax to filtering out desserts

--- recipe_app/static/script.js
+++ recipe_app/static/script.js
@@ -18,4 +18,25 @@ $(document).ready(function(){
 
         })
     })
+
+    $('#dessert_filter').submit(function(e){
+        console.log('PASS!')
+        e.preventDefault()
+        $.ajax({
+            url: '/dessert/filter',
+            method: 'post',
+            data: $(this).serialize(),
+
+            success: function(serverResponse){
+                console.log(serverResponse)
+            // replace everything in noteboard with the response we get (ex: serverResponse -> file we're rendring) using .html replaces html in that noteboard section
+
+                $('.dessert').html(serverResponse)
+                }
+
+                
+
+        })
+    })
+
 })
